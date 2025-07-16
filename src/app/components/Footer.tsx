50aae413c7df582f6bdbebe47516762314c63a55
Felipe Gomes da Silva <felipe.gsilva@protonmail.com>
2025-07-16 09:50:29 -0300

    coiso

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,7 +5,7 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons';
 export default function Footer() {
   return (
     <footer className="bg-main-100 text-white p-4 text-center">
-      <p className="text-sm sm:text-base">&copy; 2024 Coding Ferpa. Todos os direitos reservados.</p>
+      <p className="text-sm sm:text-base">&copy; 2025 Coding Ferpa. Todos os direitos reservados.</p>
       <p className="text-sm sm:text-base mt-2">
         Desenvolvido com <FontAwesomeIcon icon={faHeart}/> da comunidade para a comunidade.
       </p>
